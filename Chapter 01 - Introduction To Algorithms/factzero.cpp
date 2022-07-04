7925904065beea9bef22937e7cce9a8e690ff418
Aleksandar Aleksiev <alexiev_alexander@abv.bg>
2022-07-04 15:55:53 +0300

    add factzero.cpp

---
+++ Chapter 01 - Introduction To Algorithms/factzero.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+const unsigned n = 10;
+
+int main() 
+{
+    unsigned zeroes = 0;
+    unsigned p = 5;
+
+    while (n >= p) 
+    {
+        zeroes += (n / p);
+        p *= 5;
+    }
+    std::cout << "Броят на нулите в края на " << n << "! е " << zeroes << std::endl;
+
+    return 0;
+}
