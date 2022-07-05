4d199bdf1d9dffb54b95bff02fca650866c3f71b
Aleksandar Aleksiev <alexiev_alexander@abv.bg>
2022-07-05 14:40:42 +0300

    add digits.cpp

---
+++ Chapter 01 - Introduction To Algorithms/digits.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+unsigned n = 4242;
+
+int main() 
+{
+	unsigned digits = 0;
+	for (int i = n; i > 0; i /= 10)
+	{
+		++digits;
+	}
+
+	std::cout << "Броят на цифрите на " << n << " e " << digits << std::endl;
+	return 0;
+}
