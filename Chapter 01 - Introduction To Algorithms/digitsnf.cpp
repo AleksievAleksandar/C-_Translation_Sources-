75c81d9e75dde8bc2289a66bc9b602178b4aaa1f
Aleksandar Aleksiev <alexiev_alexander@abv.bg>
2022-07-05 14:52:16 +0300

    add digitsnf.cpp

---
+++ Chapter 01 - Introduction To Algorithms/digitsnf.cpp
@@ -0,0 +1,17 @@
+﻿#include <math.h>
+#include <iostream>
+
+const unsigned long n = 123;
+
+int main()
+{
+    float digits = 0;
+    for (unsigned i = 1; i <= n; i++)
+    {
+        digits += log10(i);
+    }
+
+    /* операцията [x] ще реализираме, като използваме превръщане до тип unsigned long */
+    std::cout << "Броят на цифрите на " << n << "! е " << (unsigned long)digits + 1 << std::endl;
+    return 0;
+}
