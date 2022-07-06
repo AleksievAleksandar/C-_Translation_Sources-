066c918ea76ae9d3faea16e2cd3078a0e5e9ea8b
Aleksandar Aleksiev <alexiev_alexander@abv.bg>
2022-07-06 12:03:55 +0300

    add sumzero.cpp

---
+++ Chapter 01 - Introduction To Algorithms/sumzero.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <math.h>
+
+/* Брой числа в редицата */
+constexpr size_t n = 8;
+/* Редица */
+int a[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+/* Търсена сума */
+int sum = 0;
+
+void checkSol(void)
+{
+    int tempSum = 0;
+    for (size_t i = 0; i < n; i++)
+    {
+        tempSum += a[i];
+    }
+
+    if (tempSum == sum)
+    { /* намерено е решение => отпечатваме го */
+        for (size_t i = 0; i < n; i++)
+        {
+            if (a[i] > 0)
+            {
+                std::cout << "+" << a[i] << " ";
+            }
+            else
+            {
+                std::cout << a[i] << " ";
+            }
+        }
+
+        std::cout << "= " << tempSum << std::endl;
+    }
+}
+
+void variate(unsigned i)
+{
+    if (i >= n)
+    {
+        checkSol();
+        return;
+    }
+    a[i] = abs(a[i]);
+    variate(i + 1);
+
+    a[i] = -abs(a[i]);
+    variate(i + 1);
+}
+
+int main()
+{
+    variate(0);
+    return 0;
+}
