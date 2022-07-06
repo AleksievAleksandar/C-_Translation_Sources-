684ffa50a46b8d26a09fe4cd3c8d3a64f4dc8955
Aleksandar Aleksiev <alexiev_alexander@abv.bg>
2022-07-06 13:22:37 +0300

    add matrix.cpp

---
+++ Chapter 01 - Introduction To Algorithms/matrix.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+constexpr size_t m = 5;
+constexpr size_t n = 5;
+
+int A[m][n];
+
+int main()
+{
+	/* Въвеждане по редове */
+	for (size_t i = 0; i < m; i++)
+	{
+		for (size_t j = 0; j < n; j++)
+		{
+			std::cin >> A[i][j];
+		}
+	}
+
+
+	/* Въвеждане по стълбове */
+	for (size_t i = 0; i < m; i++)
+	{
+		for (size_t j = 0; j < n; j++)
+		{
+			std::cin >> A[j][i];
+		}
+	}
+
+
+	/* Отпечатване на матрица по редове */
+	for (size_t i = 0; i < m; i++)
+	{
+		for (size_t j = 0; j < n; j++)
+		{
+			std::cout << A[i][j] << " ";
+		}
+		std::cout << std::endl;;
+	}
+
+	return 0;
+}
