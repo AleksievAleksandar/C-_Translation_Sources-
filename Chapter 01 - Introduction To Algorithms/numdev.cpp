aa686dd6aef7ec4cae063a44721b4a1316d85011
Aleksandar Aleksiev <alexiev_alexander@abv.bg>
2022-07-06 15:55:42 +0300

    add numdev.cpp

---
+++ Chapter 01 - Introduction To Algorithms/numdev.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+/* Число, което ще се разлага */
+unsigned n = 435;
+
+int main()
+{
+    unsigned how, i;
+
+    std::cout << n << " = ";
+    i = 1;
+    while (n != 1)
+    {
+        i++;
+        how = 0;
+        while ((n % i) == 0)
+        {
+            how++;
+            n = n / i;
+        }
+        for (size_t j = 0; j < how; j++)
+        {
+            std::cout << i << " ";
+        }
+    }
+
+    return 0;
+}
