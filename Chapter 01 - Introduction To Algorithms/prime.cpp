0a65bdf95dec647e2cba9864d6b6b9d4767650e9
Aleksandar Aleksiev <alexiev_alexander@abv.bg>
2022-07-07 16:19:35 +0300

    add prime.cpp

---
+++ Chapter 01 - Introduction To Algorithms/prime.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <math.h>
+#include <iostream>
+
+const unsigned n = 23;
+
+/* Функцията връща 1 ако n е просто и 0 ако n е съставно */
+char isPrime(unsigned n)
+{
+    unsigned i = 2;
+    if (n == 2)
+    {
+        return 1;
+    }
+
+    while (i <= sqrt(n))
+    {
+        if (n % i == 0)
+        {
+            return 0;
+        }
+        ++i;
+    }
+    return 1;
+}
+
+int main()
+{
+    if (isPrime(n))
+    {
+        std::cout << "Числото " << n << " е просто." << std::endl;
+    }
+    else
+    {
+        std::cout << "Числото " << n << " е съставно." << std::endl;
+    }
+    return 0;
+}
